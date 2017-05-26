bec998d599203000a104e823e20b4ab3bdbd4c3d
SotInde <sotinde@gmail.com>
2017-05-26 17:03:43 +0300

    Form Register movie added
    In this form we can add a movie and tha original database gets the update

---
+++ SakilaDBFormApp/SakilaDBFormApp/Forms/frmRegister.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SakilaDBFormApp.Forms
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReleaseYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRentalDuration;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRentalRate;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtReplacementCost;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbxLanguages;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cbxRatings;
+        private System.Windows.Forms.Button btnAdd;
+    }
+
+}
